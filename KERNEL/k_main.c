6707eabdcccf17d9e29b2f58e0c581b87f88e46f
MINA <mina@minaqwq.dev>
2024-05-21 03:02:51 +0200

    added C part

---
+++ KERNEL/k_main.c
@@ -0,0 +1,7 @@
+#include "x86.h"
+
+void
+kernel_main(void)
+{
+	kernel_hlt();
+}
---
+++ KERNEL/x86.h
@@ -0,0 +1,7 @@
+#ifndef KERNEL_X86_H
+#define KERNEL_X86_H
+
+/* disable interrupts and try to halt the cpu */
+extern void kernel_hlt();
+
+#endif
